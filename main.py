915123ca45cbfe20942555c3c97e291d0545ded2
elektrox <elektrox@gmail.com>
2025-08-11 23:46:05 +0000

    changed flask parameters to serve the directory

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
+from flask import Flask, send_from_directory
 import os
-
-from flask import Flask, send_file
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return send_file('src/index.html')
+# Get the absolute path of the directory where this file is located
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+# Define the path to the 'src' directory
+SRC_DIR = os.path.join(APP_ROOT, 'src')
 
-def main():
-    app.run(port=int(os.environ.get('PORT', 80)))
+@app.route('/')
+def serve_index():
+    """Serves the index.html file."""
+    return send_from_directory(SRC_DIR, 'index.html')
 
-if __name__ == "__main__":
-    main()
+@app.route('/<path:path>')
+def serve_static(path):
+    """Serves other static files like CSS and images."""
+    return send_from_directory(SRC_DIR, path)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
